88cd3fbe212188d5d18499f1da2a533ac3cdde51
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-23 09:36:59 +0000

    test change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>This is the blogs page! Cool!</h1>
+      <h1>This is the home page. The root route.</h1>
+      <h2>This is a change to the website</h2>
     </main>
   );
 }
