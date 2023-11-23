d63bb05765e71140c2a0e6eda5ace30dc39e31af
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-23 13:54:40 +0000

    add generate static params

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,8 +1,31 @@
-export default function Page() {
+import { type } from "os";
+import React from "react";
+import { getPostsBySlug, getPosts } from "@/lib/posts";
+import { notFound } from "next/navigation";
+
+type blogPostParams = {
+  params: {
+    slug: string;
+  };
+};
+
+export function generateStaticParams() {
+  const posts = getPosts();
+  return posts.map((post) => {
+    return { slug: post.slug };
+  });
+}
+
+export default function Page({ params }: blogPostParams) {
+  const post = getPostsBySlug(params.slug);
+
+  if (!post) {
+    notFound();
+  }
   return (
     <div>
-      <h1>This is the post title</h1>
-      <p>This is the post content</p>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </div>
   );
 }
